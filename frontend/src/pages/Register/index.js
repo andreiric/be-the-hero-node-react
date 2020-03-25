ea97ce09dd1b627ecd1e20e5758a91790ebb7f53
Andrei Ricardo Gomes Marques <andrei.ric@hotmail.com>
2020-03-25 12:24:19 -0400

    Front-End do Aplicativo

---
+++ frontend/src/pages/Register/index.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import './styles.css';
+
+import logo from '../../assets/logo.svg';
+import api from '../../services/api';
+
+export default function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [whatsapp, setWhatsapp] = useState('');
+  const [city, setCity] = useState('');
+  const [uf, setUf] = useState('');
+
+  const history = useHistory();
+
+  async function handleRegister(event) {
+    event.preventDefault();
+
+    const data = {
+      name,
+      email,
+      whatsapp,
+      city,
+      uf
+    };
+
+    try {
+      await api.post('ongs', data);
+      history.push('/');
+    } catch (error) {
+      alert('Erro no cadastro, tente novamente.');
+    }
+  }
+
+  return (
+    <div className="register-container">
+      <div className="content">
+        <section>
+          <img src={logo} alt="Be The Hero" />
+
+          <h1>Cadastro</h1>
+          <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+
+          <Link className="back-link" to="/">
+            <FiArrowLeft size={16} color="#e02041" />
+            Não tenho cadastro
+          </Link>
+        </section>
+
+        <form onSubmit={handleRegister}>
+          <input type="text" placeholder="Nome da ONG" value={name} onChange={event => {setName(event.target.value)}} />
+          <input type="email" placeholder="E-mail" value={email} onChange={event => {setEmail(event.target.value)}} />
+          <input type="text" placeholder="Whatsapp" value={whatsapp} onChange={event => {setWhatsapp(event.target.value)}} />
+
+          <div className="input-group">
+            <input type="text" placeholder="Cidade" value={city} onChange={event => {setCity(event.target.value)}} />
+            <input type="text" placeholder="UF" style={{ width: 80 }} value={uf}
+              onChange={event => {setUf(event.target.value)}} />
+          </div>
+
+          <button type="submit" className="button">Cadastrar</button>
+        </form>
+      </div>
+    </div>
+  );
+}
